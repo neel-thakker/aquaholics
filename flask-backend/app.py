cf59ad9ac45cd7e5d3d8c539f1a2d2a725d13980
pra17dod <pra17dod@gmail.com>
2023-02-18 23:20:22 +0530

    add volume and rsi in indicator.py

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
-from indicator_data import get_indicator_data
+from indicator_data import get_indicator_data, get_indicator_info
 
 app = Flask(__name__)
 CORS(app)
@@ -31,7 +31,18 @@ def get_data_m():
     period = path.get('period')
     print(name, ticker, interval, period)
     df = get_indicator_data(name, ticker, interval, period)
-    print(df)
+    return jsonify(df)
+
+@app.route('/getIndicatorInfo', methods=['GET'])
+def get_data_ind():
+    path = request.args
+    name = path.get('name')
+    ticker = path.get('ticker') + '.NS'
+    interval = path.get('interval')
+    period = path.get('period')
+    indicator = path.get('indicator')
+    print(name, ticker, interval, period, indicator)
+    df = get_indicator_info(name, ticker, interval, period, indicator)
     return jsonify(df)
 
 if __name__ == '__main__':
--- flask-backend/indicator_data.py
+++ flask-backend/indicator_data.py
@@ -33,11 +33,8 @@ def get_sma(stock,period):
     return SMA
 
 def get_ema(stock, period):
-    exp = stock['close'].ewm(span = period, adjust = False, min_periods = period).mean()
-    ema_df = pd.DataFrame(exp)
-    col_name = 'EMA_' + str(period)
-    ema_df.columns = [col_name]
-    return ema_df
+    EMA = stock['close'].ewm(span = period, adjust = False, min_periods = period).mean()
+    return EMA
 
 def get_atr(stock):
     ATR = talib.ATR(stock['high'], stock['low'], stock['close'], timeperiod=14)
@@ -51,35 +48,119 @@ def get_adl(stock):
     AD = talib.AD(stock['high'], stock['low'], stock['close'], stock['Volume'])
     return AD
 
-def get_all_data(name, interval, period):
-    stock = get_indicator_data(name, interval, period)
-    print(stock)
-    print(get_adl(stock))
-    print(get_adx(stock))
-    print(get_atr(stock))
-    print(get_bollinger_bands(stock, rate=20))
-    print(get_ema(stock, 20))
-    print(get_macd(stock))
-    print(get_rsi(stock, 14))
-    print(get_sma(stock, 20))
+def get_sma_slope(sma,period):
+    slope = (sma.iloc[-1] - sma.iloc[-period]) / period
+    return slope
+
+def get_adl_slope(adl,period):
+    slope = (adl.iloc[-1] - adl.iloc[-period]) / period
+    return slope
+
+def get_indicator_info(name, ticker, interval, period, indicator):
+    tk = yf.Ticker(ticker)
+    data=tk.history(period=period, interval=interval)
+    data.rename(columns = {'Open':'open','Close':'close','High':'high','Low':'low'}, inplace = True)
+
+
+    if indicator[0:3] == "SMA":
+        func = get_sma
+        if indicator[3:5] == "20":
+            period = 20
+        elif indicator[3:5] == "50":
+            period = 50
+        elif indicator[3:6] == "100":
+            period = 100
+
+    elif indicator[0:3] == "EMA":
+        func = get_ema
+        if indicator[3:5] == "20":
+            period = 20
+        elif indicator[3:5] == "50":
+            period = 50
+        elif indicator[3:6] == "100":
+            period = 100
+
+    val = func(data, period)
+    result = val.to_json(orient="index")
+    parsed = json.loads(result)
+
+    df = []
+    for items in parsed:
+        df.append({ "x" : int(items), "y" : parsed[items]})
+
+    ctx = {
+        'name': name,
+        'ticker': ticker,
+        'data': df
+    }
+    return ctx
+
+def get_analysis(data):
+    #get_macd(data)
+    sma20=get_sma(data,20)
+    sma100=get_sma(data,100)
+    #get_bollinger_bands(data)
+    adx=get_adx(data)
+    #get_ema(data,20)
+    #get_atr(data)
+    adl=get_adl(data)
+    res = []
+    sma20S=get_sma_slope(sma20,5)
+    sma100S=get_sma_slope(sma100,10)
+    adlS=get_adl_slope(adl,5)
+    if(data['rsi'].iloc[-1] > 70):
+        res.append("The value of RSI have exeeded 70. This shows that the stock is overbought and its price is expected to drop. This can be considered as a sell signal.")
+    elif(data['rsi'].iloc[-1] < 30):
+        res.append("The value of RSI have dropped below 30. This shows that the stock is oversold and its price is expected to increase. This can be considered as a buy signal.")
+    else:
+        res.append("RSI value is oscillating in middle showing no specifi trend. Thus, currently investors should avoid taking any positions.")
+
+    if(adx.iloc[-1]>75):
+        res.append("The value of ADX is very high indicating that the current trend is very strong and the price is expected to follow the current trend. Currently investors can take positions with low risk.")
+    elif(adx.iloc[-1]>50):
+        res.append("The current trend is moderatly strong. The price is expected to follow the current trend but can reverse in near future.")
+    else:
+        res.append("The value of ADX is very low indicating that the stock price is not following any trend. Investors should avoid taking any positions in current situation.")
+
+    if(sma20S<0):
+        res.append("Short term moving averages are indicating a downward trend in the share price.")
+    else:
+        res.append("Short term moving averages are indicating a upward trend in the share price.")
+    if(sma100S>0):
+        res.append("Long term moving averages are indicating a upward trend in the share price.")
+    else:
+        res.append("Long term moving averages are indicating a downward trend in the share price.")
+
+    if(adlS<0 and sma20S<0):
+        res.append("The ADL indicator is indicating that the downward trend is expected to continue and the price of share would further decrease. Investors should take a sell position.")
+    elif(adlS>0 and sma20S<0):
+        res.append("The ADL indicator is indicating that there may be buying pressure and the price trend is expected to reverse. The price of share is expected to rise in near future.")
+    elif(adlS<0 and sma20S>0):
+        res.append("The ADL indicator is indicating that there may be selling pressure and the price trend is expected to reverse. The price of share is expected to fall in near future.")
+    else:
+        res.append("The ADL indicator is indicating that the upward trend is expected to continue and the price of share would further increase. Investors should take a buy position.")
+    return res
+
 
 def get_indicator_data(name, ticker, interval, period):
     tk = yf.Ticker(ticker)
     data=tk.history(period=period, interval=interval)
     data.rename(columns = {'Open':'open','Close':'close','High':'high','Low':'low'}, inplace = True)
+    rsi_val = get_rsi(data, 14)['rsi']
+    data['rsi'] = rsi_val
     result = data.to_json(orient="index")
     parsed = json.loads(result)
-    # return parsed
-    # print(type(parsed))
-    data = []
+    df = []
     for items in parsed:
-        ohlc = [parsed[items]['open'], parsed[items]['high'], parsed[items]['low'], parsed[items]['close']]
-        data.append({ "x" : int(items), "y" : ohlc})
+        val = [parsed[items]['open'], parsed[items]['high'], parsed[items]['low'], parsed[items]['close']]
+        df.append({ "x" : int(items), "y" : val, "v" : parsed[items]['Volume'], "r": parsed[items]['rsi']})
 
     open_val = round(tk.fast_info.open, 2)
     curr_val = round(tk.fast_info.last_price, 2)
     chg_val = round(curr_val - open_val , 2)
     percent_chg = round(((chg_val/open_val) * 100),2)
+
+    anal_data = get_analysis(data)
 
     ctx = {
         'name': name,
@@ -87,10 +168,11 @@ def get_indicator_data(name, ticker, interval, period):
         'currVal': curr_val,
         'changeVal': chg_val,
         'percentChange': percent_chg,
-        'data': data
+        'data': df,
+        'analysis': anal_data
     }
     return ctx
 
 
 if __name__ == "__main__":
-    print(get_indicator_data(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
+    print(get_indicator_info(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]))
